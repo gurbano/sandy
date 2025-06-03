40060fca4df7230fbfce2603a35ae6fbc9b2f978
gurbano <g.urbano@gmail.com>
2025-06-04 00:22:16 +0100

    scaffold

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+import logo from "./assets/logo.png"; // Import the logo image
+const App = () => {
+    return (_jsxs("div", { children: [_jsx("h1", { children: "Hello, React + TypeScript + Rollup!" }), _jsx("img", { src: logo, alt: "Logo" }), " "] }));
+};
+export default App;
